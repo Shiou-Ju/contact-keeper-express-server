8c76f91bf52df7f2e9addf297a4e6b059d95f167
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-09-24 12:59:52 +0800

    Done setting types and ContactState

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,46 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      { id: 1, name: "J11", email: "11@11.com", phone: "11111", type: "個人" },
+      { id: 2, name: "22", email: "22@22.com", phone: "22222", type: "個人" },
+      { id: 3, name: "33", email: "33@33.com", phone: "33333", type: "工作" },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // add contact
+  // delete contact
+  // update contact
+  // filter contacts
+  // clear filter
+  // set current contact
+  // clear current contact
+
+  return (
+    <ContactContext.Provider
+      // anything to be accessed from other component
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
